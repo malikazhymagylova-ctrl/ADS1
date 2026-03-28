78550c5005a378d3046541bc13ad4b7e707ab8b2
Zhumagulova Malika <malikazhymagylova@gmail.com>
2026-03-28 23:07:17 +0600

    Assignment 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    class ADS1 {
 
-    for (int i = 1; i <= 15; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        System.out.println("Task 1: " + sumSquares(4));
+
+        int[] arr = {1, 2, 3, 4};
+        System.out.println("Task 2: " + sumArray(arr, arr.length));
+
+        System.out.println("Task 3: " + sumPowers(2, 3));
+
+        System.out.print("Task 4: ");
+        reverseArray(arr, arr.length - 1);
+    }
+
+    public static int sumSquares(int n) {
+        if (n == 1) return 1;
+        return n * n + sumSquares(n - 1);
+    }
+
+    public static int sumArray(int[] arr, int n) {
+        if (n == 0) return 0;
+        return arr[n - 1] + sumArray(arr, n - 1);
+    }
+
+    public static int sumPowers(int a, int n) {
+        if (n == 1) return a;
+        return (int)Math.pow(a, n) + sumPowers(a, n - 1);
+    }
+
+    public static void reverseArray(int[] arr, int index) {
+        if (index < 0) return;
+        System.out.print(arr[index] + " ");
+        reverseArray(arr, index - 1);
     }
 }
